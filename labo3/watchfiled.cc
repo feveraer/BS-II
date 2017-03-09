b11fed2656bf8f35719561af6abeb3269ce2b705
root <frederic.everaert@gmail.com>
2017-03-09 11:40:25 +0100

    watchfiled.cc updated

--- labo3/watchfiled.cc
+++ labo3/watchfiled.cc
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fstream>
@@ -13,23 +14,29 @@ int main(int argc, char **argv) {
   std::ifstream watchfile("/root/c/labo3/watchfile.txt");
   const char* watch = "/root/c/labo2/watchfile";
   std::map<std::string, bool> is_watched_map;
+  std::string single;
 
   // read file to get all files that need to be watched,
   // initialize map that nothing is being watched yet
-  std::string single;
+  // not necessary
   // while (watchfile && getline(watchfile, single)) {
   //   is_watched_map[single] = false;
   // }
 
   while(1) {
-    while (watchfile && getline(watchfile, single)) {
+    while (getline(watchfile, single)) {
       is_watched_map[single] = false;
-      if (is_watched_map.find(single) != is_watched_map.end()) {
+      if (!is_watched_map[single]) {
         if (fork() == 0) {
           execl(watch, watch, single.c_str(), (char*) 0);
           is_watched_map[single] = true;
         }
       }
+      watchfile.clear(); // clear EOF flag
+      watchfile.seekg(0, std::ios::beg);
+
+      // delay for 1 second
+      sleep(1);
     }
   }
   return 0;
